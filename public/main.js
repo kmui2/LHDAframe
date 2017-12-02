b3804fe27cdbbabb85c3941dcbd85fd82c98b795
kmui2 <kmui2@wisc.edu>
2017-12-02 14:33:58 -0600

    fixed url param functionality

--- public/main.js
+++ public/main.js
@@ -1,32 +1,28 @@
 $(document).ready(function () {
-	var getUrlParameter = function getUrlParameter(sParam) {
-		var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-			sURLVariables = sPageURL.split('&'),
-			sParameterName,
-			i;
-	
-		for (i = 0; i < sURLVariables.length; i++) {
-			sParameterName = sURLVariables[i].split('=');
-	
-			if (sParameterName[0] === sParam) {
-				return sParameterName[1] === undefined ? true : sParameterName[1];
-			}
+	$.urlParam = function(name){
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		if (results==null){
+		   return null;
 		}
-	};
+		else{
+		   return decodeURI(results[1]) || 0;
+		}
+	}
 	// var boxColors = ['yellow','black','red','white','blue','green','brown','orange','violet'];
+	console.log($.urlParam('box1'));
 
 	$('#marker').html(`
-		<a-box position='-0.5 0.5 -0.5' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box1')||'yellow'};'></a-box>
-            <a-box position='0 0.5 -0.5' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box2')||'black'};'></a-box>
-            <a-box position='0.5 0.5 -0.5' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box3')||'red'};'></a-box>
+		<a-box position='-0.5 0.5 -0.5' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box1')||'yellow'};'></a-box>
+            <a-box position='0 0.5 -0.5' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box2')||'black'};'></a-box>
+            <a-box position='0.5 0.5 -0.5' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box3')||'red'};'></a-box>
 
-            <a-box position='-0.5 0.5 0' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box4')||'white'};'></a-box>
-            <a-box position='0 0.5 0' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box5')||'blue'};'></a-box>
-            <a-box position='0.5 0.5 0' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box6')||'green'};'></a-box>
+            <a-box position='-0.5 0.5 0' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box4')||'white'};'></a-box>
+            <a-box position='0 0.5 0' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box5')||'blue'};'></a-box>
+            <a-box position='0.5 0.5 0' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box6')||'green'};'></a-box>
 
             
-            <a-box position='-0.5 0.5 0.5' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box7')||'brown'};'></a-box>
-            <a-box position='0 0.5 0.5' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box8')||'orange'};'></a-box>
-            <a-box position='0.5 0.5 0.5' width='0.25' height='0.25' depth='0.25' material='color: ${getUrlParameter('box9')||'violet'};'></a-box>
+            <a-box position='-0.5 0.5 0.5' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box7')||'brown'};'></a-box>
+            <a-box position='0 0.5 0.5' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box8')||'orange'};'></a-box>
+            <a-box position='0.5 0.5 0.5' width='0.25' height='0.25' depth='0.25' material='color: ${'#'+$.urlParam('box9')||'violet'};'></a-box>
 	`)
 });
